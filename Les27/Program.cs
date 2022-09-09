9062e92a3a93b2e68c5c1bfd6a7744f912e8e47b
Vrezh <vrezhikmarkaryan@gmail.com>
2022-09-09 22:51:30 +0500

    Les27

---
+++ Les27/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Задача 27. Выдаёт сумму цифр в числе");
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+do 
+{
+    sum = sum + numberN % 10;
+    numberN = numberN / 10;
+}
+while (numberN > 0 );
+{
+Console.WriteLine(sum);
+}
